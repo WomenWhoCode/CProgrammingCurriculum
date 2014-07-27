b8ac387b6b90086ac978ecbe61d3d745bc434e87
Flarnie Marchan <fnonemaker@gmail.com>
2014-07-27 11:16:54 -0700

    Add function to ask player if they hit

--- games/blackjack/basic_blackjack.c
+++ games/blackjack/basic_blackjack.c
@@ -14,6 +14,34 @@ int card_value(char card_code[3])
   return atoi(card_code);
 }
 
+int valid_input(char user_input[2])
+{
+  char input = user_input[0];
+  if ((input != 'y') && (input != 'n'))
+    return 0;
+  return 1;
+}
+
+int parse_user_input(char user_input[2])
+{
+  char input = user_input[0];
+  if (input == 'y')
+    return 1;
+  if (input == 'n')
+    return 0;
+  return 500;
+}
+
+int will_hit()
+{
+  char user_input[2];
+  printf("Will you hit?");
+  fgets(user_input, sizeof(user_input), stdin);
+  if (valid_input(user_input))
+    return parse_user_input(user_input);
+  return 500;
+}
+
 int main()
 {
   /* Play blackjack! (Coming soon.) */
@@ -36,6 +64,12 @@ int main()
 
   puts("Card values for AH, QH, and 4H should be 11, 10, and 4: ");
   printf("%i, %i, %i", ace_value, queen_value, four_value);
+
+  /* Testing the "will_hit" function */
+  if (will_hit())
+    puts("You will hit!");
+  else
+    puts("You will not hit.");
 
   return 0;
 }
